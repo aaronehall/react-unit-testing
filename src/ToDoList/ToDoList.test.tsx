f0da4f7a10424d243df579c9ebad57ad73e40d12
blakemccracken <blake.mccracken@integrate.com>
2022-04-25 10:43:59 -0700

    adding basic features with tests

--- src/ToDoList/ToDoList.test.tsx
+++ src/ToDoList/ToDoList.test.tsx
@@ -2,11 +2,12 @@ import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import { ToDoList } from "./ToDoList";
 import * as toDoService from "../services/toDoService";
 import { faker } from "@faker-js/faker";
+import userEvent from "@testing-library/user-event";
 
 describe("ToDoList", () => {
     it("should load list items", () => {
         // Arrange
-        const toDoItems = [ faker.lorem.word(), faker.lorem.word() ];
+        const toDoItems = [faker.lorem.word(), faker.lorem.word()];
         jest.spyOn(toDoService, "getToDoList").mockReturnValue(toDoItems);
 
         // Act
@@ -46,6 +47,85 @@ describe("ToDoList", () => {
         // Assert
         await waitFor(() => {
             expect(screen.queryByText(itemToDelete)).not.toBeInTheDocument();
+        });
+    });
+
+    it("should display the number of to-do items", async () => {
+        // Arrange
+        const toDoItems = [faker.lorem.word(), faker.lorem.word()];
+        jest.spyOn(toDoService, "getToDoList").mockReturnValue(toDoItems);
+        render(<ToDoList />);
+
+        expect(screen.getByText("Number of To-Do List items: 2")).toBeInTheDocument();
+
+        // Act
+        const newToDoItem = faker.lorem.word();
+        const input = screen.getByLabelText("todo-input");
+        fireEvent.change(input, { target: { value: newToDoItem } });
+        fireEvent.click(screen.getByText("Add To-Do Item"));
+
+        // Assert
+        await waitFor(() => {
+            expect(screen.getByText("Number of To-Do List items: 3")).toBeInTheDocument();
+        });
+    });
+
+    it("should display a message when the to-do list is complete", async () => {
+        // Arrange
+        const expectedText = "You did everything on your list!";
+        const itemToDelete = faker.lorem.word();
+        jest.spyOn(toDoService, "getToDoList").mockReturnValue([itemToDelete]);
+        render(<ToDoList />);
+
+        // Act
+        fireEvent.click(screen.getByLabelText(`delete-${itemToDelete}-0`));
+
+        // Assert
+        await waitFor(() => {
+            expect(screen.getByText(expectedText)).toBeInTheDocument();
+        });
+    });
+
+    it("should hide the complete message when the 'OK' button is clicked", async () => {
+        // Arrange
+        const expectedText = "You did everything on your list!";
+        const itemToDelete = faker.lorem.word();
+        jest.spyOn(toDoService, "getToDoList").mockReturnValue([itemToDelete]);
+        render(<ToDoList />);
+
+        // Act
+        fireEvent.click(screen.getByLabelText(`delete-${itemToDelete}-0`));
+        fireEvent.click(screen.getByRole('button', { name: 'OK' }))
+
+        // Assert
+        await waitFor(() => {
+            expect(screen.queryByText(expectedText)).not.toBeInTheDocument();
+        });
+    });
+
+    it("should show error text when an empty item is attempted to be added", async () => {
+        // Arrange
+        const expectedErrorText = "You must enter something";
+        const toDoItems = [faker.lorem.word(), faker.lorem.word()];
+        jest.spyOn(toDoService, "getToDoList").mockReturnValue(toDoItems);
+        render(<ToDoList />);
+
+        // Act
+        const input = screen.getByLabelText("todo-input");
+        userEvent.paste(input, "");
+        userEvent.click(screen.getByText("Add To-Do Item"));
+
+        // Assert
+        await waitFor(() => {
+            expect(screen.getByText(expectedErrorText)).toBeInTheDocument();
+        });
+
+        // Act
+        userEvent.paste(input, "Something");
+
+        // Assert
+        await waitFor(() => {
+            expect(screen.queryByText(expectedErrorText)).not.toBeInTheDocument();
         });
     });
 });
--- src/ToDoList/ToDoList.tsx
+++ src/ToDoList/ToDoList.tsx
@@ -1,36 +1,70 @@
-import { useState, ChangeEvent } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 import { getToDoList } from "../services/toDoService";
 
 export function ToDoList() {
     const [toDoList, setToDoList] = useState<string[]>(getToDoList());
     const [newToDoItem, setNewToDoItem] = useState<string>("");
-  
+    const [shouldShowCompleteMessage, setShouldShowCompleteMessage] = useState<boolean>(false);
+    const [shouldShowErrorText, setShouldShowErrorText] = useState<boolean>(false);
+
     const onNameChange = (e: ChangeEvent<HTMLInputElement>) => {
-      e.preventDefault();
-      const newNameValue = e.currentTarget.value;
-      setNewToDoItem(newNameValue);
+        e.preventDefault();
+        const newNameValue = e.currentTarget.value;
+        setNewToDoItem(newNameValue);
     }
-  
+
     const handleAddItem = () => {
-      setToDoList([...toDoList, newToDoItem]);
+        if (newToDoItem === "") {
+            setShouldShowErrorText(true)
+            return;
+        }
+        setToDoList([...toDoList, newToDoItem]);
     }
 
     const handleDeleteItem = (itemName: string) => {
-      const newToDoList = toDoList.filter(i => i !== itemName);
-      setToDoList(newToDoList);
+        const newToDoList = toDoList.filter(i => i !== itemName);
+        setToDoList(newToDoList);
     }
-  
+
+    const handleCompleteModalClose = () => {
+        setShouldShowCompleteMessage(false);
+    }
+
+    useEffect(() => {
+        if (toDoList.length === 0) {
+            setShouldShowCompleteMessage(true)
+        }
+    }, [toDoList])
+
+    useEffect(() => {
+        if (shouldShowErrorText && newToDoItem.length > 0) {
+            setShouldShowErrorText(false);
+        }
+    }, [newToDoItem])
+
     return (
-      <div>
-        <h1>To-Do List</h1>
-        <input aria-label="todo-input" onChange={value => onNameChange(value)} /> {" "}
-        <button onClick={() => handleAddItem()}>Add To-Do Item</button>
-        <ul>
-          {toDoList.map((name, index) => 
-            <li key={`li-${index}`}>{`${name} `} 
-              <button aria-label={`delete-${name}-${index}`} key={`button-${index}`} onClick={() => handleDeleteItem(name)}>X</button>
-            </li>)}
-        </ul>
-      </div>
+        <>
+            <div>
+                <h1>To-Do List</h1>
+                {shouldShowErrorText && <p style={{color: "red"}}>You must enter something</p>}
+                <input aria-label="todo-input" onChange={value => onNameChange(value)}/> {" "}
+                <button onClick={handleAddItem}>Add To-Do Item</button>
+                <ul>
+                    {toDoList.map((name, index) =>
+                        <li key={`li-${index}`}>{`${name} `}
+                            <button aria-label={`delete-${name}-${index}`} key={`button-${index}`}
+                                    onClick={() => handleDeleteItem(name)}>X
+                            </button>
+                        </li>)}
+                </ul>
+                <hr/>
+                <h3>Number of To-Do List items: {toDoList.length}</h3>
+            </div>
+
+            {shouldShowCompleteMessage && <div>
+                <p>You did everything on your list!</p>
+                <button onClick={handleCompleteModalClose}>OK</button>
+            </div>}
+        </>
     );
 }
