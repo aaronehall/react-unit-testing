05d5fac8d7870ca087b667d6d4c318e4b882f91b
Aaron Hall <Aaron.Hall@integrate.com>
2022-04-29 10:27:24 -0500

    Add integration test for AddItem component

--- src/components/ToDoList/ToDoList.test.tsx
+++ src/components/ToDoList/ToDoList.test.tsx
@@ -1,9 +1,10 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import { ToDoList } from "./ToDoList";
 import * as toDoService from "../../services/toDoService";
 import { faker } from "@faker-js/faker";
 
 describe("ToDoList", () => {
+    // true unit test
     it("should load list items", () => {
         // Arrange
         const toDoItems = [ faker.lorem.word(), faker.lorem.word() ];
@@ -15,5 +16,25 @@ describe("ToDoList", () => {
         // Assert
         expect(screen.getByText(toDoItems[0])).toBeInTheDocument();
         expect(screen.getByText(toDoItems[1])).toBeInTheDocument();
-    });    
+    });
+
+    // integration test: List component + Add component
+    it("should allow the user to add an item", async () => {
+        // Arrange
+        const toDoItems = [ faker.lorem.word(), faker.lorem.word() ];
+        jest.spyOn(toDoService, "getToDoList").mockReturnValue(toDoItems);
+        const expectedNewItem = faker.lorem.word();
+
+        // Act
+        render(<ToDoList />);
+
+        const input = screen.getByLabelText("todo-input");
+        fireEvent.change(input, { target: { value: expectedNewItem } });
+        fireEvent.click(screen.getByText("Add To-Do Item"));
+
+        // Assert
+        await waitFor(async () => {
+            expect(await screen.findByText(expectedNewItem)).toBeInTheDocument();
+        });
+    });
 });
