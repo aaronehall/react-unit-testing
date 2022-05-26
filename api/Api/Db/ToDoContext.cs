5c48e510fceaaeb1339e55773a4896fea59c89dd
Aaron Hall <Aaron.Hall@integrate.com>
2022-05-26 15:32:19 -0500

    Seed DB.

--- api/Api/Db/ToDoContext.cs
+++ api/Api/Db/ToDoContext.cs
@@ -19,5 +19,27 @@ namespace Api.Db
         {
             options.UseSqlite($"Data Source={DbPath}");
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ToDoItem>().HasData(new List<ToDoItem>
+            {
+                new()
+                {
+                    Id = 1,
+                    Description = "Pick up groceries"
+                },
+                new()
+                {
+                    Id = 2,
+                    Description = "Go to bank"
+                },
+                new()
+                {
+                    Id = 3,
+                    Description = "Go to post office"
+                }
+            });
+        }
     }
 }
