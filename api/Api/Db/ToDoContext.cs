75521e8271b84baa419079dc400f65b3f69d0b53
Aaron Hall <Aaron.Hall@integrate.com>
2022-05-26 15:28:02 -0500

    Add DB for API

---
+++ api/Api/Db/ToDoContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Api.Db
+{
+    public class ToDoContext : DbContext
+    {
+        public DbSet<ToDoItem> ToDoItems { get; set; }
+
+        public string DbPath { get; }
+
+        public ToDoContext()
+        {
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(folder);
+            DbPath = Path.Combine(path, "todo.db");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlite($"Data Source={DbPath}");
+        }
+    }
+}
---
+++ api/Api/Db/ToDoItem.cs
@@ -0,0 +1,8 @@
+﻿namespace Api.Db
+{
+    public class ToDoItem
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+    }
+}
